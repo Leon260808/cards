c228d0bc82438337d1ce979db877389c6bedebc7
Leon Tran <leon_tran@sluz.ch>
2026-04-24 12:59:33 +0200

    WIP

--- cards.ts
+++ cards.ts
@@ -1,3 +1,30 @@
-export function hello(): string {
-  return "world";
+export enum Colors {
+    Rosen,
+    Schellen,
+    Schilten,
+    Eicheln,
+}
+
+export enum Values {
+    Sechs,
+    Sieben,
+    Acht,
+    Neun,
+    Banner,
+    Under,
+    Ober,
+    König,
+    Ass,
+}
+
+export class Card {
+    constructor(readonly color: Colors, readonly value: Values) {}
+
+    beats(other: Card): boolean {
+        if (this.value !== other.value) {
+            return this.value > other.value;
+        }
+        return (this.color === Colors.Rosen && other.color === Colors.Schellen) ||
+               (this.color === Colors.Schilten && other.color === Colors.Eicheln);
+    }
 }
---
+++ deck.ts
@@ -0,0 +1,3 @@
+class Deck {
+    let iterator =
+}
